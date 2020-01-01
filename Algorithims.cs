a79cb4126c43e0a767ab2c26b82f36cd09966e01
JamesRileydev <jamesriley.dev@gmail.com>
2019-12-31 21:06:18 -0500

    First commit

---
+++ Algorithims.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AandDcourse
+{
+    class Algorithims
+    {
+        public void FizzBuzz(int n)
+        {
+            var str = "";
+
+            for (int i = 1; i <= n; i++)
+            {
+                str = (i % 5 == 0 && i % 3 == 0) ? "fizzbuzz" : i % 3 == 0 ? "buzz" : i % 5 == 0 ? "fizz" : $"{i}";
+                Console.WriteLine(str);
+            }
+        }
+
+        public char MaxChars(string s)
+        {
+            var d = new Dictionary<char, int>();
+
+            foreach (var ch in s)
+            {
+                if (!d.ContainsKey(ch))
+                {
+                    d[ch] = 1;
+                }
+                else
+                {
+                    d[ch]++;
+                }
+            }
+
+            var max = d.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+
+            Console.WriteLine(max);
+
+            return max;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AandDcourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var algo = new Algorithims();
+
+            algo.MaxChars("string 1111223");
+
+            algo.FizzBuzz(15);
+        }
+    }
+}
